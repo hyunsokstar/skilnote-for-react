82dfee9bd8d9e68980b567a4f59f067129c6109d
unknown <hyunsokstar@google.com>
2020-12-22 02:30:20 +0900

    update for deploy 2

--- accounts2/models.py
+++ accounts2/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
-from todo.models import TeamInfo
 
 # Create your models here.
 class Profile(models.Model):
@@ -13,7 +12,6 @@ class Profile(models.Model):
     reputation = models.IntegerField(default=0)
     shortcut_user_id = models.CharField(default="me", max_length=40)
     selected_category_id = models.IntegerField(default=1, blank=True)
-    team = models.ForeignKey(TeamInfo, on_delete=models.CASCADE, null=True, blank=True)
     position = models.CharField(max_length=50,default="member")
     subject_of_memo = models.CharField(max_length=60) # 스킬 노트의 주제
     skill_note_reputation = models.IntegerField(default=0) # skill note 유저 리스트 점수 추가할때 계산됨
